76a1851d23cf1ac5b25d05d3f17db525215c7031
Heitor Piva Carreira <heitorpiva015@gmail.com>
2022-11-19 16:26:28 -0300

    Alterado a tela de lista de veiculos

--- lib/View/VehicleActive/vehicle_active_view.dart
+++ lib/View/VehicleActive/vehicle_active_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:gestao_web/View/CollaboratorRegistration/collaborator_registration.dart';
 import 'package:gestao_web/View/VehicleActive/vehicle_active_container.dart';
 import 'package:gestao_web/Theme/theme.dart';
 import 'package:gestao_web/Widgets/export_all_widget.dart';
+import 'package:gestao_web/View/export_all_view.dart';
 
 
 class VehicleActiveView extends StatefulWidget {
@@ -13,30 +15,54 @@ class VehicleActiveView extends StatefulWidget {
 }
 
 class _VehicleActiveViewState extends State<VehicleActiveView> {
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  final PageController _pageController = PageController();
-  int indexNavigatorBar = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      extendBody: true,
-      backgroundColor: const Color.fromARGB(255, 240, 240, 240),
-      appBar: const CustomAppBarHome(
-        title: "Lista de Veiculos",
-      ),
-      body: PageView(
-        onPageChanged: (int page) {
-          setState(() {
-            indexNavigatorBar = page;
-          });
-        },
-        controller: _pageController,
-        children: [
-          VehicleActiveContainer(uid: widget.uid),
-        ],
-      ),
-    );
+        backgroundColor: const Color.fromARGB(255, 240, 240, 240),
+        appBar: const CustomAppBarHome(
+          title: "Lista de Veiculos",
+        ),
+        body: Row(children: <Widget>[
+          Column(
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 15, 10, 30),
+                child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => Home(),
+                      ));
+                    },
+                    icon: Icon(Icons.people_alt_outlined),
+                    label: Text("Colaboradores                 ")),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 30),
+                child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => VehicleRegistration(),
+                      ));
+                    },
+                    icon: Icon(Icons.fire_truck),
+                    label: Text("Cadastro de veiculo        ")),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 30),
+                child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => CollaboratorRegistration(),
+                      ));
+                    },
+                    icon: Icon(Icons.addchart_outlined),
+                    label: Text("Cadastro de Colaborador")),
+              ),
+            ],
+          ),
+          const VerticalDivider(thickness: 1, width: 1),
+          Expanded(child: VehicleActiveContainer(uid: widget.uid)),
+        ]));
   }
 }

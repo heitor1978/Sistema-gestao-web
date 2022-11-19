42cc181890c1073338d10f2aacc83a79237ccd14
Heitor Piva Carreira <heitorpiva015@gmail.com>
2022-11-19 16:51:25 -0300

    Tela de ocorrencia de motorista alterado

--- lib/View/CollaboratorOccurrence/collaborator_occurrence_view.dart
+++ lib/View/CollaboratorOccurrence/collaborator_occurrence_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gestao_web/View/CollaboratorOccurrence/collaborator_occurrence_container.dart';
+import 'package:gestao_web/View/CollaboratorRegistration/collaborator_registration.dart';
 import 'package:gestao_web/View/export_all_view.dart';
 import 'package:gestao_web/Widgets/export_all_widget.dart';
 
@@ -18,23 +19,49 @@ class _CollaboratorOccurrenceState extends State<CollaboratorOccurrence> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      extendBody: true,
-      backgroundColor: const Color.fromARGB(255, 240, 240, 240),
-      drawer: CustomDrawer(),
-      appBar: CustomAppBarHome(title: "Ocorrências dos Funcionários"),
-
-      body: PageView(
-        onPageChanged: (int page) {
-          setState(() {
-            indexNavigatorBar = page;
-          });
-        },
-        controller: _pageController,
-        children: [
-          CollaboratorOccurrenceContainer(uid: widget.uid),
-        ],
-      ),
-    );
+        backgroundColor: const Color.fromARGB(255, 240, 240, 240),
+        appBar: CustomAppBarHome(title: "Ocorrências dos Funcionários"),
+        body: Row(children: <Widget>[
+          Column(
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 15, 10, 30),
+                child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => Home(),
+                      ));
+                    },
+                    icon: Icon(Icons.people_alt_outlined),
+                    label: Text("Colaboradores                 ")),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 30),
+                child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => VehicleRegistration(),
+                      ));
+                    },
+                    icon: Icon(Icons.fire_truck),
+                    label: Text("Cadastro de veiculo        ")),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 0, 10, 30),
+                child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => CollaboratorRegistration(),
+                      ));
+                    },
+                    icon: Icon(Icons.addchart_outlined),
+                    label: Text("Cadastro de Colaborador")),
+              ),
+            ],
+          ),
+          const VerticalDivider(thickness: 1, width: 1),
+          Expanded(child: CollaboratorOccurrenceContainer(uid: widget.uid),)
+        ]));
   }
 }
